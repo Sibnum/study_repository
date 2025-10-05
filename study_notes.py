db539f882b1f605b3ebeffae18dbe6a7c0e692d8
Igor Zinchenko <sibnum@yandex.ru>
2025-10-05 09:39:17 +0300

    test class BacteriaProducer

---
+++ study_notes.py
@@ -0,0 +1,36 @@
+class BacteriaProducer:
+    """ Инициализация конвеера бактерий"""
+    def __init__(self, max_bacteria):
+        self.max_bacteria = max_bacteria
+        current_bacteria_count = 0
+        self.current_bacteria_count = current_bacteria_count
+
+    # Допишите метод, добавляющий бактерию в чашу
+    # Если уже достигнуто максимальное число бактерий,
+    # то добавлять новую нельзя, выведите сообщение об ошибке
+    def create(self):
+        if self.current_bacteria_count != self.max_bacteria:
+            self.current_bacteria_count += 1
+            print (f'Добавлена одна бактерия. Бактерий в колонии: {self.current_bacteria_count}')
+        else:
+            print (f'Нет места под новую бактерию')
+
+    # Допишите метод, удаляющий бактерию из чаши.
+    # Если в чаше уже нет бактерий, то удалять нечего;
+    # выведите сообщение об ошибке
+    def delete(self):
+        if self.current_bacteria_count > 0:
+            self.current_bacteria_count -= 1
+            print (f'Одна бактерия удалена. Бактерий в колонии: {self.current_bacteria_count}')
+        else:
+            print (f'В популяции нет бактерий, удалять нечего')
+
+
+# Пример запуска для самопроверки
+bacteria_producer = BacteriaProducer(max_bacteria=3)
+bacteria_producer.delete()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.create()
+bacteria_producer.delete()
