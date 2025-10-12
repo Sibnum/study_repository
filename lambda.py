de18dc5a4d0cf199d5fe032122701ac1d2eed607
Igor Zinchenko <sibnum@yandex.ru>
2025-10-12 21:23:25 +0300

    task lambda is done

--- lambda.py
+++ lambda.py
@@ -1,13 +1,14 @@
 people = ['Антон', 'Соня', 'Коля', 'Женя', 'Тоня', 'Стёпа']
 
+
 def say_to_all(func, sequence):
     for item in sequence:
         func(item)
 
 
-# Этот вызов для каждого имени из списка должен напечатать
-# строчку Привет, <имя>!
-say_to_all(lambda item: print(f'Привет, {item}!'), people)
-# Этот вызов для каждого имени из списка должен напечатать
-# строчку До завтра, <имя>!
-say_to_all(lambda item: print(f'До завтра, {item}!'), people)
+say_to_all(
+    lambda item:
+    print(f'Здравствуй, {item}!') 
+    if item[0] == 'С' 
+    else print(f'Привет, {item}!'),
+    people)
