5bfc7447c0800af9f9d9ce85602f82e4856cb188
Igor Zinchenko <sibnum@yandex.ru>
2025-10-12 14:32:26 +0300

    task is done

--- def choose_days.py
+++ def choose_days.py
@@ -4,22 +4,22 @@ from random import sample
 
 def choose_days():
     # Определяем диапазон дат первой половины месяца.
-    first_month_half = list(range(0, 15))
+    first_month_half = list(range(1, 16))
 
     # Выбор трёх случайных чисел:
     random_days = sample(first_month_half, k=3)
     # Cортировка этих чисел по возрастанию:
-    sorted_days = sorted(random_days)
+    sorted_days = sorted(random_days) #sorted_days = [4, 5, 6]
 
     # Получаем сегодняшнюю дату.
     # На её основе будут генерироваться даты для занятий:
     now = datetime.now()
 
     # Чтобы было проще формировать сообщение, начнём цикл с 1.
-    for i in range(1, 4):
+    for i in range(0, 3):
         # Генерируем дату занятия, подменяя номер дня в сегодняшней дате.
-        practice_day = now.replace(day=sorted_days[i]).strftime("%d.%M.%Y")
-        print(f'{i}-е занятие: {practice_day}')
+        practice_day = now.replace(day=sorted_days[i]).strftime("%d.%m.%Y")
+        print(f'{i + 1}-е занятие: {practice_day}')
 
 
 choose_days()
---
+++ generator.py
@@ -0,0 +1,3 @@
+numbers = [1, 3, 4, 6, 9, 11]
+
+print(sum(num**2 for num in numbers if num % 3 == 0))
