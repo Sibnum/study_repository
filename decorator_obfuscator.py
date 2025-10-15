dcc6ed6ebb7fbdee6d405b0be7c6b4078ea79d42
Igor Zinchenko <sibnum@yandex.ru>
2025-10-15 19:44:59 +0300

    task is done

--- decorator_obfuscator.py
+++ decorator_obfuscator.py
@@ -1,5 +1,13 @@
-# Напишите декоратор obfuscator
-...
+def obfuscator(func):
+    def wrapper():
+        result_func = func()
+        name = result_func['name']
+        password = result_func['password']
+        new_name = name[:1] + '*******' + name[-1:]
+        new_password = '*' * len(password)
+        return {'name': new_name, 'password': new_password}
+    return wrapper
+
 
 @obfuscator
 def get_credentials():
