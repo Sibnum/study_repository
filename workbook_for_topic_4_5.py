fd9879bfed68417bd4cf453f66254783477179f1
Igor Zinchenko <sibnum@yandex.ru>
2025-11-18 22:13:06 +0300

    task in progress

---
+++ workbook_for_topic_4_5.py
@@ -0,0 +1,28 @@
+# Произведите необходимые импорты и установите точность вычислений
+
+# Объявите функцию conversion_result() с указанными в задаче параметрами
+
+    # Инициализируйте переменную now текущим временем
+
+	# Исходя из условия задачи верните нужную строку
+
+# Объявите функцию universal_converter() с указанными в задаче параметрами
+
+	# Сделайте проверку вводных параметров
+
+	# Исходя из введенного кода страны вызовите функцию conversion_result() с параметрами соответствующими этому коду
+def conversion_result(code, rub_amount=0):
+    if rub_amount < 0:
+        return f'Ошибка ввода: rub_amount {rub_amount}'
+    correct_codes = {'DE', 'US', 'CNN', 'de', 'us', 'cnn'}
+    if not code in correct_codes:
+        return f'Ошибка ввода: code {code}'
+    
+
+
+
+print(universal_converter('DE')) # Местная дата и время: 21.08.2025 23:24 Uhr, местный курс к рублю: 93.763
+print(universal_converter('DE', 100)) # Местная дата и время: 21.08.2025 23:24 Uhr, 100 RUB = 1.07 EUR
+print(universal_converter('De', 100)) # Ошибка ввода: code De
+print(universal_converter('us', 322)) # Местная дата и время: 21/08/2025 23:24, 322 RUB = 4.06 USD
+print(universal_converter('cnn', -1)) # Ошибка ввода: rub_amount -1
