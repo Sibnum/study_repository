6e89a3926bf3cd67247a013e0babccba55fe42ae
Igor Zinchenko <sibnum@yandex.ru>
2025-11-22 23:04:04 +0300

    task in progress

--- workbook_for_topic_4_5.py
+++ workbook_for_topic_4_5.py
@@ -1,5 +1,3 @@
-
-	# Исходя из введенного кода страны вызовите функцию conversion_result() с параметрами соответствующими этому коду
 from datetime import datetime
 from decimal import Decimal, getcontext
 
@@ -8,42 +6,56 @@ def universal_converter(code, rub_amount=0):
     if rub_amount < 0:
         return f'Ошибка ввода: rub_amount {rub_amount}'
     correct_codes = {'DE', 'US', 'CNN', 'de', 'us', 'cnn'}
-    if not code in correct_codes:
+    if code not in correct_codes:
         return f'Ошибка ввода: code {code}'
-    conversion_result(dt_format, rub_amount)
+    now = datetime.now()
+    currency_symbol = code
+    if code == 'DE' or code == 'de':
+        dt_format = now.strftime("%d.%m.%Y %H:%M Uhr")
+        currency_rate = 93.763
+    elif code == 'US' or code == 'us':
+        dt_format = now.strftime("%d/%m/%Y %H:%M")
+        currency_rate = 79.322
+    elif code == 'CNN' or code == 'cnn':
+        dt_format = now.strftime("%d-%m-%Y %H:%M")
+        currency_rate = 11.164
     if rub_amount == 0:
+        now = now.strftime("%d.%m.%Y %H:%M")
         return (
             f'Местная дата и время: {now}, '
-            f'местный курс к рублю: {conversion_result(code)}'
+            f'местный курс к рублю: {currency_rate}'
         )
-    elif code == 'DE' or code == 'de':
-        now_de = now.strftime("%d.%m.%Y %H:%M")
-        return (
-            f'Местная дата и время: {now_de} Uhr, '
-            f'{rub_amount} RUB = {conversion_result(code)} EUR'
+    else:
+        result = conversion_result(
+            dt_format, currency_rate, currency_symbol, rub_amount
         )
-    elif code == 'US' or code == 'us':
-        now_us = now.strftime("%d/%m/%Y %H:%M")
-        return (
-            f'Местная дата и время: {now_us}, '
-            f'{rub_amount} RUB = {conversion_result(code)} USD'
-        )
-    elif code == 'CNN' or code == 'cnn':
-        now_cnn = now.strftime("%d-%m-%Y %H:%M")
-        return (
-            f'Местная дата и время: {now_cnn}, '
-            f'{rub_amount} RUB = {conversion_result(code)} CNN'
-        )
-    
-def conversion_result(dt_format,currency_rate, currency_symbol, rub_amount=0):
-    
-    
-    
-now = datetime.now()
-getcontext().prec = 2
+        return result
 
-print(universal_converter('DE')) # Местная дата и время: 21.08.2025 23:24 Uhr, местный курс к рублю: 93.763
-print(universal_converter('DE', 100)) # Местная дата и время: 21.08.2025 23:24 Uhr, 100 RUB = 1.07 EUR
-print(universal_converter('De', 100)) # Ошибка ввода: code De
-print(universal_converter('us', 322)) # Местная дата и время: 21/08/2025 23:24, 322 RUB = 4.06 USD
-print(universal_converter('cnn', -1)) # Ошибка ввода: rub_amount -1
+
+def conversion_result(dt_format, currency_rate, currency_symbol, rub_amount=0):
+    getcontext().prec = 3
+    if currency_symbol == 'DE' or currency_symbol == 'de':
+        result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
+        return (
+            f'Местная дата и время: {dt_format}, '
+            f'{rub_amount} RUB = {result_dec} EUR'
+        )
+    elif currency_symbol == 'US' or currency_symbol == 'us':
+        result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
+        return (
+            f'Местная дата и время: {dt_format}, '
+            f'{rub_amount} RUB = {result_dec} USD'
+        )
+    elif currency_symbol == 'CNN' or currency_symbol == 'cnn':
+        result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
+        return (
+            f'Местная дата и время: {dt_format}, '
+            f'{rub_amount} RUB = {result_dec} CNN'
+        )
+
+
+print(universal_converter('DE'))  # Местная дата и время: 21.08.2025 23:24 Uhr, местный курс к рублю: 93.763
+print(universal_converter('DE', 100))  # Местная дата и время: 21.08.2025 23:24 Uhr, 100 RUB = 1.07 EUR
+print(universal_converter('De', 100))  # Ошибка ввода: code De
+print(universal_converter('us', 322))  # Местная дата и время: 21/08/2025 23:24, 322 RUB = 4.06 USD
+print(universal_converter('cnn', -1))  # Ошибка ввода: rub_amount -1
