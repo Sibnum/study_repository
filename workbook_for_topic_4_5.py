0f301ff0f0f3fdaacc026d6b888bf4a8ee99451f
Igor Zinchenko <sibnum@yandex.ru>
2025-11-23 08:53:35 +0300

    task is done

--- workbook_for_topic_4_5.py
+++ workbook_for_topic_4_5.py
@@ -9,20 +9,22 @@ def universal_converter(code, rub_amount=0):
     if code not in correct_codes:
         return f'Ошибка ввода: code {code}'
     now = datetime.now()
-    currency_symbol = code
     if code == 'DE' or code == 'de':
         dt_format = now.strftime("%d.%m.%Y %H:%M Uhr")
         currency_rate = 93.763
+        currency_symbol = 'EUR'
     elif code == 'US' or code == 'us':
         dt_format = now.strftime("%d/%m/%Y %H:%M")
         currency_rate = 79.322
+        currency_symbol = 'USD'
     elif code == 'CNN' or code == 'cnn':
         dt_format = now.strftime("%d-%m-%Y %H:%M")
         currency_rate = 11.164
+        currency_symbol = 'CNY'
     if rub_amount == 0:
         now = now.strftime("%d.%m.%Y %H:%M")
         return (
-            f'Местная дата и время: {now}, '
+            f'Местная дата и время: {dt_format}, '
             f'местный курс к рублю: {currency_rate}'
         )
     else:
@@ -34,23 +36,23 @@ def universal_converter(code, rub_amount=0):
 
 def conversion_result(dt_format, currency_rate, currency_symbol, rub_amount=0):
     getcontext().prec = 3
-    if currency_symbol == 'DE' or currency_symbol == 'de':
+    if currency_symbol == 'EUR':
         result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
         return (
             f'Местная дата и время: {dt_format}, '
-            f'{rub_amount} RUB = {result_dec} EUR'
+            f'{rub_amount} RUB = {result_dec} {currency_symbol}'
         )
-    elif currency_symbol == 'US' or currency_symbol == 'us':
+    elif currency_symbol == 'USD':
         result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
         return (
             f'Местная дата и время: {dt_format}, '
-            f'{rub_amount} RUB = {result_dec} USD'
+            f'{rub_amount} RUB = {result_dec} {currency_symbol}'
         )
-    elif currency_symbol == 'CNN' or currency_symbol == 'cnn':
+    elif currency_symbol == 'CNY':
         result_dec = Decimal(str(rub_amount)) / Decimal(str(currency_rate))
         return (
             f'Местная дата и время: {dt_format}, '
-            f'{rub_amount} RUB = {result_dec} CNN'
+            f'{rub_amount} RUB = {result_dec} {currency_symbol}'
         )
 
 
